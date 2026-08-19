03ab31f02a9a0ab87867c52c38255e44d42729c2
faikureturk-collab <faikureturk@gmail.com>
2026-08-19 11:26:30 +0300

    fiyat fix

--- src/components/PricingSection.tsx
+++ src/components/PricingSection.tsx
@@ -37,8 +37,8 @@ export default function PricingSection({
               }`}
             >
               <span
-                className={`absolute top-1 h-5 w-5 rounded-full bg-white shadow transition-transform ${
-                  yearly ? "translate-x-6" : "translate-x-1"
+                className={`absolute left-1 top-1 h-5 w-5 rounded-full bg-white shadow transition-transform ${
+                  yearly ? "translate-x-5" : "translate-x-0"
                 }`}
               />
             </button>
